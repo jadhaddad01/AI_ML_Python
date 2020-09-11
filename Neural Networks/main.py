c401fe49639c260880df31916dd4230ede58896a
jadhaddad01 <jadhaddad01@protonmail.com>
2020-09-11 16:26:02 -0400

    Neural Networks Start

---
+++ Neural Networks/main.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+print(train_images[7])
+
+plt.imshow(train_images[7], cmap=plt.cm.binary)
+plt.show()
